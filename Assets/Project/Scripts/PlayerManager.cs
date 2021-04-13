6269b0df851a3c3865164300ff0c44af08b8d463
Alexey Dmitriev <a.dmitriev@level26.games>
2021-04-13 18:38:38 +0300

    First load project

---
+++ Assets/Project/Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeTeam
+{
+    CT, T
+}
+
+public class PlayerManager : MonoBehaviour
+{
+    public int hp = 100;
+    public int armor = 100;
+    public TypeTeam typeTeam;
+
+    //Получение урона
+    public void DealDamage(int damage)
+    {
+        //TODO: РАССЧИТАТЬ УРОН АРМОРА И ЖИЗНЕЙ
+
+        if (armor == 0)
+        {
+            hp -= damage;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/ScriptableObjects/Gun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CS2D/New gun")]
+public class Gun : ScriptableObject
+{
+    public string nameGun;
+    public int countAmmo;
+    public GameObject prefabBullet;
+    public int damage;
+    public float speedBullet;
+}
